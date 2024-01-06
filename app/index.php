b671b26dfa1c2e805ce29c0961c7437a9a5ad4d0
Udara Nalawansa <udarakavishka13@gmail.com>
2024-01-06 21:59:46 +0530

    Update Index

--- app/index.php
+++ app/index.php
@@ -1,35 +1,11 @@
 <?php
-    require("../lib/auth.php");
+require("../lib/auth.php");
 
-    $username = $_SESSION['username'];
-    $user_id = $_SESSION['user_id'];
+$username= $_SESSION['username'];
+$user_id= $_SESSION['user_id'];
 
-    require("../lib/config.php");
+require("../lib/config.php");
 
-    $notes = array();
-
-    $sql = "SELECT * FROM notes WHERE user_id = $user_id ORDER BY created_at DESC";
-    $result = $conn->query($sql);
-
-    if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            $note = array(
-                "id" => $row["id"],
-                "title" => $row["title"],
-                "content" => $row["content"],
-                "created_at" => $row["created_at"]
-            );
-
-            array_push($notes, $note);
-        }
-    } else {
-        $error = "Create a note to get started!";
-    }
-
-    function truncate($string, $length)
-    {
-        return (strlen($string) > $length) ? substr($string, 0, $length) . "..." : $string;
-    }
 ?>
 
 <!DOCTYPE html>
