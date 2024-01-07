782c79653fe8d6ecfc09421f894771bb7636df6f
DilmyPerera <samadiniperera20@gmail.com>
2024-01-07 11:24:11 +0530

    error fixed

--- login.php
+++ login.php
@@ -1,7 +1,8 @@
 <?php
  session_start();
 
- if($_SERVER['REQUSET METHOD'] === 'POST'){
+ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
     $username = $_POST['username'];
     $password = $_POST['password'];
     $authenticated = false;
